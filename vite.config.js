cc00cd326414a04e48a88c6c639bd5a1120d6dd4
Aniket5353 <darkpanda44788@gmail.com>
2026-07-06 12:29:08 +0530

    asdf

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
+console.log("vite is loading ")
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  // base: "/Impact-Employer-Dashboard/",
+  base: "/Impact-Employer-Dashboard/",
   server: {
     port: 5173,
   },
