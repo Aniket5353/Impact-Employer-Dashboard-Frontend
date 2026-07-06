0a644cce7101022f0fd093f67c250a5a9317f632
Aniket5353 <darkpanda44788@gmail.com>
2026-07-06 12:51:23 +0530

    Remove node_modules cache from repository

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-console.log("vite is loading ")
-
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/Impact-Employer-Dashboard/",
-  server: {
-    port: 5173,
-  },
+  base: "/Impact-Employer-Dashboard-Frontend/",
 });
